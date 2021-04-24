d1afed03ab771731b12157a6c098a6bb2f7b130d
Erica Gillis <erica_gillis1@baylor.edu>
2021-04-24 15:37:20 -0500

    add new entries

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,8 +1,17 @@
 from django import forms 
 from .models import Topic
+from .models import Entry
 
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
         label = {'Text: ':''}
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ["text"]
+        label = {'Text: ':''}
+        widgets = {'text':forms.Textarea(attrs={'cols':80})}
+        
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 # Create your views here.
 def index(request):
     ''' The home page for Learning Log.'''
@@ -35,3 +35,20 @@ def new_topic(request):
 
     context = {'form': form}
     return render(request, 'MainApp/new_topic.html', context)
+
+def new_entry(request,topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+
+            return redirect('MainApp:topic', topic_id= topic_id)
+
+    context = {'form': form, 'topic': topic}
+    return render(request, 'MainApp/new_entry.html', context)
